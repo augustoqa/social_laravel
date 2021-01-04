bbc6bc997684eff518a1b22ad040338635b67b4c
Cesar Acual <augustoacual@gmail.com>
2021-01-04 16:55:11 -0600

    Hacer que la prueba para que un usuario autenticado pueda crear estados pase

--- tests/Feature/CreateStatusTest.php
+++ tests/Feature/CreateStatusTest.php
@@ -9,20 +9,25 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CreateStatusTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * @test
      */
     public function an_authenticated_user_can_create_statuses()
     {
+        $this->withoutExceptionHandling();
+
         // 1. Given => Teniendo un usuario autenticado
         $user = factory(User::class)->create();
         $this->actingAs($user);
 
         // 2. When => Cuando hace un post request a status
-        $this->post(route('status.store'), ['body' => 'Mi primer status']);
+        $this->post(route('statuses.store'), ['body' => 'Mi primer status']);
 
         // 3. Then => Entonces veo un nuevo estado en la base de datos
         $this->assertDatabaseHas('statuses', [
+            'user_id' => $user->id,
             'body' => 'Mi primer status'
         ]);
     }
