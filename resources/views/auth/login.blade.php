fbf0a08ed6a56628d0de72b2fd5ea86d3421fb90
Cesar Acual <augustoacual@gmail.com>
2021-01-08 19:25:20 -0600

    Diseñando el formulario de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,27 @@
-<form action="{{ route('login') }}" method="post">
-    @csrf
-    <input type="email" name="email">
-    <input type="password" name="password">
-    <button id="login-btn">Login</button>
-</form>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 mx-auto">
+                <div class="card border-0 bg-light px-4 py-2">
+                    <form action="{{ route('login') }}" method="post">
+                        @csrf
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="email">Email:</label>
+                                <input class="form-control border-0" type="email" name="email" id="email" placeholder="Tu email...">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password:</label>
+                                <input class="form-control border-0" type="password" name="password" id="password" placeholder="Tu contraseña">
+                            </div>
+                            <button class="btn btn-primary btn-block" id="login-btn">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::view('/', 'welcome');
+Route::view('/', 'welcome')->name('home');
 
 Route::post('statuses', 'StatusesController@store')
     ->name('statuses.store')
