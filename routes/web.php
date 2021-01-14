341357f4be1fce95f3c00ca47cdd020744a563b6
Cesar Acual <augustoacual@gmail.com>
2021-01-14 16:59:49 -0600

    Probar listado de estados

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 Route::view('/', 'welcome')->name('home');
 
+Route::get('statuses', 'Statusescontroller@index')
+    ->name('statuses.index');
 Route::post('statuses', 'StatusesController@store')
     ->name('statuses.store')
     ->middleware('auth');
