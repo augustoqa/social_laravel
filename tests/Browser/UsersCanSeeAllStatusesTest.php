c9979ff4c679470aa7f9fcd9076d2bf8227a4dc8
Cesar Acual <augustoacual@gmail.com>
2021-01-14 18:26:07 -0600

    Probando el listado de estados con Dusk

---
+++ tests/Browser/UsersCanSeeAllStatusesTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\Status;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class UsersCanSeeAllStatusesTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function users_can_see_all_statuses_on_the_home()
+    {
+        $statuses = factory(Status::class, 3)->create();
+
+        $this->browse(function (Browser $browser) use ($statuses) {
+            $browser->visit('/')
+                    ->waitForText($statuses->first()->body)
+                    ->assertSee($statuses->first()->body);
+
+            foreach ($statuses as $status) {
+                $browser->assertSee($status->body);
+            }
+        });
+    }
+}
